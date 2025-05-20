73edbe6eafb78a6017c2b2460deb78e1ea5ad02e
richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-05-20 15:33:35 -0300

    adicionando variaveis de ambiente

--- src/main/java/br/com/richard/blog/config/Security.java
+++ src/main/java/br/com/richard/blog/config/Security.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class Security {
@@ -13,8 +14,12 @@ public class Security {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/artigos/**").authenticated()
+                .requestMatchers(HttpMethod.PUT, "/artigos/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/artigos/**").authenticated()
                 .anyRequest().permitAll()
-            );
+            )
+            .httpBasic();
         return http.build();
     }
 }
