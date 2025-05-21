a18a7ce837f5ee16775b91600a49be89bfaf5d33
Richard Almeida <128538196+Richardalmeida21@users.noreply.github.com>
2025-05-21 09:55:38 -0300

    Update ArtigoController.java

--- src/main/java/br/com/richard/blog/controller/ArtigoController.java
+++ src/main/java/br/com/richard/blog/controller/ArtigoController.java
@@ -1,7 +1,7 @@
 package br.com.richard.blog.controller;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
