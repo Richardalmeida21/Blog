00cdbfd25055724fc905e1d5f9d6fd1136fe479e
richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-05-21 11:55:24 -0300

    adicionando id

--- src/main/java/br/com/richard/blog/service/ArtigoService.java
+++ src/main/java/br/com/richard/blog/service/ArtigoService.java
@@ -2,10 +2,8 @@ package br.com.richard.blog.service;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.richard.blog.dto.ArtigoRequestDto;
 import br.com.richard.blog.dto.ArtigoResponseDto;
 import br.com.richard.blog.model.Artigo;
