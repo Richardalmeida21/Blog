8744c0323bc8f17ad3eaa285eb2eb92011044262
richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-05-21 16:26:04 -0300

    v3 para postgres

--- src/main/resources/db/migration/V3__add_column_codigo.sql
+++ src/main/resources/db/migration/V3__add_column_codigo.sql
@@ -1,3 +1,3 @@
 ALTER TABLE artigos ADD COLUMN codigo VARCHAR(10);
 ALTER TABLE artigos ADD CONSTRAINT artigos_codigo_uk UNIQUE (codigo);
-UPDATE artigos SET codigo = 'A' || TO_CHAR(FLOOR(DBMS_RANDOM.VALUE(100000, 999999))) WHERE codigo IS NULL;
+UPDATE artigos SET codigo = 'A' || floor(random() * 900000 + 100000)::text WHERE codigo IS NULL;
