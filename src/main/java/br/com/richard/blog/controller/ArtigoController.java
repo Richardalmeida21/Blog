07fdcb2fdad432da9527d7a963616aaf34141a2e
richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-05-21 12:05:22 -0300

    Endpoint de teste

--- src/main/java/br/com/richard/blog/controller/ArtigoController.java
+++ src/main/java/br/com/richard/blog/controller/ArtigoController.java
@@ -1,16 +1,18 @@
 package br.com.richard.blog.controller;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import br.com.richard.blog.dto.ArtigoRequestDto;
 import br.com.richard.blog.dto.ArtigoResponseDto;
 import br.com.richard.blog.model.Categoria;
@@ -28,6 +30,11 @@ public class ArtigoController {
     public ResponseEntity<Void> criarArtigo(@RequestBody ArtigoRequestDto artigoDto) {
         artigoService.criarArtigo(artigoDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "API version 2.0"; // Mude para qualquer versão para confirmar o deploy
     }
 
     @GetMapping
